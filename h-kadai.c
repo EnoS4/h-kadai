9ff4237e0949e691d813e65cad3aefccd9fd26af
EnoS4 <saya.loveup.99@gmail.com>
2016-04-16 18:11:05 +0900

    Update h-kadai.c

--- h-kadai.c
+++ h-kadai.c
@@ -2,18 +2,21 @@
 #include<string.h>
 
 int main(void) {
-  int i,j;
-  srand((unsigned) time(NULL));
-    printf("%d\n", rand()%10);
-  // ここで乱数1を発生し、aにおく
+  //int i,j;
+  srand(time(NULL));
+  int a = rand() % 10;  // ここで乱数1を発生し、aにおく
+  printf("%d\n", a);
+  
   //a < b = 0;
   //a > b =1;
   printf("高ければ1を。低ければ0を入力してください。\n");
-    gets(j);
+    scanf("%d", j);
     
-    srand((unsigned) time(NULL));
-    printf("%d\n", rand()%10);  //乱数2を発生し、bにおく
-    if (( a < b ) == j) {
+    int b = rand() % 10;  //乱数2を発生し、bにおく
+    //printf("%d\n", b);
+    if (( a < b && j == 0 ) || ( a > b && j == 1 )) {
+      
+    } else if (( a < b && j == 1 ) || ( a > b && j == 0)) {
       
     }
   return 0;
