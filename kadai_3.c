7346de59eee49d38f5a10969e5b275a0aefc4f2b
EnoS4 <saya.loveup.99@gmail.com>
2016-05-28 21:19:00 +0900

    Update kadai_3.c

--- kadai_3.c
+++ kadai_3.c
@@ -15,61 +15,65 @@ int enzan();
 int kekka(int num1, int num2, char arithmetic);
 
 int main(void) {
-	 int input;
-	 
-	 printf("数当てだよ！\n");
-	
-	while(1){
+	int input;
+
+	printf("数当てだよ！\n");
+
+	while (1) {
 		int num1 = number();
 		int num2 = number();
 		char arithmetic = enzan();
 		int result = kekka(num1, num2, arithmetic);
-	
-		printf("%d %s %d =\n", num1, arithmetic, num2);
-	  
-	  	printf("計算結果を入力してください\n");
-	  	scanf("%d", &input);
-	  	printf("%d\n", input);
-	
-	//入力した値と計算結果の比較、判定
-	    if (input == result) {
-	      printf("= %d\n正解！\n",result);
-	    } else if (input != result) {
-	      printf("= %d\n不正解！\n",result);
-	      break;
-	    }
+
+		printf("%d %c %d =\n", num1, arithmetic, num2);
+
+		printf("計算結果を入力してください\n");
+		scanf_s("%d", &input);
+
+		//入力した値と計算結果の比較、判定
+		if (input == result) {
+			printf("= %d\n正解！\n", result);
+		}
+		else if (input != result) {
+			printf("= %d\n不正解！\n", result);
+			break;
+		}
 	}
 	return 0;
 }
 
 int number() //数字をランダムで発生
 {
-  srand((unsigned)time(NULL));
-  return rand() % max;
+	srand((unsigned)time(NULL));
+	return rand() % max;
 }
 
 int enzan()  //+,=,*,/をランダムで発生
 {
- 	char *enzanshi[] = {"+","-","*","/"};
-  	srand((unsigned)time(NULL));
-  	return *enzanshi[rand() % word];
+	char enzanshi[] = { '+','-','*','/' };
+	srand((unsigned)time(NULL));
+	return enzanshi[rand() % word];
 }
 
 int kekka(int num1, int num2, char arithmetic)  //ランダムで発生させた値の計算結果
 {
 	int goukei = 0;
-	if ( arithmetic == "+") {
-  		goukei = num1 + num2;
-  	} else if (arithmetic == "-") {
+	if (arithmetic == '+') {
+		goukei = num1 + num2;
+	}
+	else if (arithmetic == '-') {
 		goukei = num1 - num2;
-  	} else if (arithmetic == "*") {
-    	goukei = num1 * num2;
-  	} else if (arithmetic == "/"){
-  	    if (num2 == 0) {
-    		/*エラー処理*/
-    		return 0;
-    	}
-    	goukei = num1 / num2;
-  	}
-  	return goukei;
+	}
+	else if (arithmetic == '*') {
+		goukei = num1 * num2;
+	}
+	else if (arithmetic == '/') {
+		if (num2 == 0) {
+			/*エラー処理*/
+			number();
+			return 0;
+		}
+		goukei = num1 / num2;
+	}
+	return goukei;
 }
